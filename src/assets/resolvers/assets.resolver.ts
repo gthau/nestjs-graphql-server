432d332012019c7282e497b11e0411cdcf375e85
Ghislain Thau <gthau@dalet.com>
2021-11-28 20:38:23 +0100

    refactored asset query to use DataLoader

--- src/assets/resolvers/assets.resolver.ts
+++ src/assets/resolvers/assets.resolver.ts
@@ -1,9 +1,18 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
-import { pipe } from 'fp-ts/lib/function';
+import { Args, Context, Query, Resolver } from '@nestjs/graphql';
+import DataLoader from 'dataloader';
+import * as E from 'fp-ts/lib/Either';
+import { flow, pipe } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/lib/Option';
 import * as T from 'fp-ts/lib/Task';
 import * as TE from 'fp-ts/lib/TaskEither';
-import { AllAssetsResult, AssetInput, AssetResult, AssetsInput, AssetsResult } from 'src/schema.gql';
+import {
+  AllAssetsResult,
+  Asset,
+  AssetInput,
+  AssetResult,
+  AssetsInput,
+  AssetsResult,
+} from 'src/schema.gql';
 import { AssetsService } from '../assets.service';
 
 @Resolver()
@@ -11,39 +20,19 @@ export class AssetsResolver {
   constructor(private readonly assetsService: AssetsService) {}
 
   @Query()
-  asset(@Args('input') { symbol }: AssetInput): Promise<AssetResult> {
-    //// option1: null when asset not found
-    // return pipe(
-    //   this.assetsService.get(symbol),
-    //   TE.map(O.toNullable),
-    //   TE.foldW(
-    //     (e) => T.of({ error: e.message }),
-    //     (a) => T.of({ asset: a }),
-    //   ),
-    // )();
-
-    //// option2: default asset when asset not found
-    // return pipe(
-    //   this.assetsService.get(symbol),
-    //   TE.map(O.getOrElseW(() => this.getDefaultAsset())),
-    //   TE.foldW(
-    //     (e) => T.of({ error: e.message }),
-    //     (a) => T.of({ asset: a }),
-    //   ),
-    // )();
-
-    // option3: AssetNotFound type when asset not found
+  async asset(
+    @Args('input') { symbol }: AssetInput,
+    @Context('assetsLoader') assetsLoader: DataLoader<string, E.Either<Error, O.Option<Asset>>>,
+  ): Promise<AssetResult> {
     return pipe(
-      this.assetsService.get(symbol),
+      () => assetsLoader.load(symbol),
       TE.foldW(
         (e) => T.of({ error: e.message }), // AssetError
-        (oa) =>
-          pipe(
-            oa,
-            O.map((a) => ({ asset: a })), // AssetResponse
-            O.getOrElseW(() => ({ symbol })), // AssetNotFound
-            T.of,
-          ),
+        flow(
+          O.map((a) => ({ asset: a })), // AssetResponse
+          O.getOrElseW(() => ({ symbol })), // AssetNotFound
+          T.of,
+        ),
       ),
     )();
   }
@@ -77,13 +66,4 @@ export class AssetsResolver {
       ),
     )();
   }
-
-  // private getDefaultAsset(): Asset {
-  //   return {
-  //     id: '0',
-  //     name: 'Unknown coin',
-  //     slug: 'unknowncoin',
-  //     symbol: 'UKNC',
-  //   };
-  // }
 }
